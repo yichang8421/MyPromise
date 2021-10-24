2067de87159dce6b92b92c97f037c620f05d5125
andy8421 <yichang8421@qq.com>
2021-10-24 20:49:42 +0800

    各个任务执行时机

--- myPromise.js
+++ myPromise.js
@@ -1,7 +1,7 @@
 const p = new Promise((resolve, reject) => {
-    console.log("执行excutor（执行器）");
-    // excutor 先执行，说明执行器函数是同步回调
-    // excutor 执行完毕，得到一个 promise 对象 p，同时开启异步任务
+    console.log("执行eexcutor（执行器）");
+    // executor 先执行，说明执行器函数是同步回调
+    // executor 执行完毕，得到一个 promise 对象 p，同时开启异步任务
     setTimeout(()=>{
         const m = Math.random();
         if (m > 0.5) {
@@ -17,7 +17,7 @@ console.log("执行其他任务");
 // then 方法指定异步回调函数 resolve 和 reject
 // 此时是在异步启动之后指定的回调，因此并不知道异步任务执行结果是成功还是失败
 // 将计时器设置在 1s 以后，则可以确定此时异步任务的执行结果
-// 即先拿到异步任务执行结果，过了一段时间才指定回调函数
+// 即 Promise 可以允许先拿到异步任务执行结果，过一段时间再指定回调函数
 setTimeout(()=>{
     p.then((value) => {
         console.log(value)

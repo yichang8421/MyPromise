bd4915c776d6b1c3ae8d6a9b471e4667bd42e46f
andy8421 <yichang8421@qq.com>
2021-10-24 22:21:02 +0800

    控制异步回调的执行时机

--- myPromise.js
+++ myPromise.js
@@ -1,21 +1,36 @@
-const p1 = new Promise((resolve, reject) => {
+// 先指定回调函数，再执行回调
+new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve(1);
+        resolve(1)  // 改变 promise 的状态，同时异步执行回调
+    }, 1000)
+}).then((value) => {    // 先指定回调函数，promise 状态此时不清楚，因此将回调函数保存起来
+    console.log("onResolved1", value);
+}, (reason) => {
+    console.log("onRejected1", reason);
+})
+
+
+// 先改变 promise 对象的状态，再指定回调函数
+new Promise((resolve, reject) => {
+    resolve(1)      // 先改变 promise 状态，指定回调参数
+}).then((value) => {    // 然后指定回调函数，同时异步执行回调函数
+    console.log("onResolved2", value);
+}, (reason) => {
+    console.log("onRejected2", reason);
+})
+
+
+// 先改变 promise 对象的状态，再指定回调函数
+const p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(1)  // 改变 promise 的状态，指定回调参数
     }, 1000)
 })
 
-const p2 = Promise.resolve(2)
-
-const p3 = new Promise((resolve,reject)=>{
-    throw new Error("出错了")
-})
-
-Promise
-    .all([p1,p2,p3])
-    // .race([p3, p2, p1])
-    .then((value) => {
-        console.log(`all sucess: ${value}`);
+setTimeout(() => {  // 等 promise 状态改变之后，指定回调函数，同时异步执行回调函数
+    p.then((value) => {
+        console.log("onResolved3", value);
+    }, (reason) => {
+        console.log("onRejected3", reason);
     })
-    .catch((reason) => {
-        console.log(`fail: ${reason}`);
-    })
+}, 1100)

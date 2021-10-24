9ec6bf2b9e6c694a820d37b58f47f56616512e32
andy8421 <yichang8421@qq.com>
2021-10-24 23:51:44 +0800

    中断 promise 链

--- myPromise.js
+++ myPromise.js
@@ -5,9 +5,9 @@ new Promise((resolve, reject) => {
     })
     .then(() => {
     }, reason => {
-        return new Promise((resolve, reject) => {
-            reject(reason)
-        })
+        // 返回一个状态为 padding 的 promise 对象，则由于状态不确定，回调函数无法触发执行。
+        // promise 链在此处被中断
+        return new Promise(()=>{})
     })
     .then(() => {
     }, reason => {

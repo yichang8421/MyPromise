d66f41af8ab33c17d9d390ba3d3b771334eb93f9
andy8421 <yichang8421@qq.com>
2021-10-24 22:34:16 +0800

    控制异步回调的执行时机

--- myPromise.js
+++ myPromise.js
@@ -1,9 +1,18 @@
+/*
+ 触发 promise 执行回调函数的条件：
+ 1. promise 对象状态发生改变
+ 2. 指定了回调函数
+
+ promise 一定是 异步地 执行回调函数
+*/
+
+
 // 先指定回调函数，再执行回调
 new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve(1)  // 改变 promise 的状态，同时异步执行回调
+        resolve(1)  // 改变 promise 的状态，同时异步执行回调函数。
     }, 1000)
-}).then((value) => {    // 先指定回调函数，promise 状态此时不清楚，因此将回调函数保存起来
+}).then((value) => {    // 先指定回调函数，promise 状态此时不清楚，因此将回调函数保存起来，等状态确定后异步地执行回调函数。
     console.log("onResolved1", value);
 }, (reason) => {
     console.log("onRejected1", reason);
@@ -12,8 +21,8 @@ new Promise((resolve, reject) => {
 
 // 先改变 promise 对象的状态，再指定回调函数
 new Promise((resolve, reject) => {
-    resolve(1)      // 先改变 promise 状态，指定回调参数
-}).then((value) => {    // 然后指定回调函数，同时异步执行回调函数
+    resolve(1)      // 先改变 promise 状态，指定回调参数（这里是同步执行）
+}).then((value) => {    // 然后then()指定回调函数（同步地）。指定回调函数之后异步地执行回调函数。
     console.log("onResolved2", value);
 }, (reason) => {
     console.log("onRejected2", reason);
@@ -27,7 +36,7 @@ const p = new Promise((resolve, reject) => {
     }, 1000)
 })
 
-setTimeout(() => {  // 等 promise 状态改变之后，指定回调函数，同时异步执行回调函数
+setTimeout(() => {  // 等 promise 状态改变之后，指定回调函数。之后再异步地执行回调函数
     p.then((value) => {
         console.log("onResolved3", value);
     }, (reason) => {

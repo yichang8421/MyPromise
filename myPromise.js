994fa53f9edaba1c2ffa423d516ca2cc0a5f304c
andy8421 <yichang8421@qq.com>
2021-10-24 23:38:57 +0800

    promise 异常传透

--- myPromise.js
+++ myPromise.js
@@ -2,20 +2,12 @@ new Promise((resolve, reject) => {
     reject(1)
 })
     .then(() => {
-    }, reason => {
-        throw reason
     })
     .then(() => {
-    }, reason => {
-        throw reason
     })
     .then(() => {
-    }, reason => {
-        throw reason
     })
     .then(() => {
-    }, reason => {
-        throw reason
     })
     .catch(reason => {
         console.log("onRejected", reason);
